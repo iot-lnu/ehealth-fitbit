8da1576d1edcd6a762f95dc24f2b3d06393e54c0
Andreas Nilsson <andreasnorje@MacBook-Pro-som-tillhor-Andreas.local>
2024-02-14 12:42:32 +0100

    Retain data before entering loop. filename constant. Code reusage for sendData().

--- fitbitLNU-app/app/index.js
+++ fitbitLNU-app/app/index.js
@@ -5,6 +5,7 @@ import * as fs from "fs";
 import * as document from "document";
 import { Accelerometer } from "accelerometer";
 import { outbox } from "file-transfer";
+import { me as device } from "device";
 
 // Set clock granularity to "seconds" for ticking every second
 clock.granularity = "seconds";
@@ -15,7 +16,10 @@ let newdata = "";
 const myLabel = document.getElementById("myLabel"); // Reference to the label element
 
 // File size limit before triggering file transfer
-const fileSizeLimit = 5000;
+// For 20 minutes reading with 5 seconds interval you have about 240 times * ~50 bytes = 12000 bytes fileSizeLimit 
+const fileSizeLimit = 2000;
+const readInterval = 5000;
+const filename = "data.txt";
 
 // Function to collect, write, and send data when the file size limit is reached
 function collectWriteAndSendData() {
@@ -37,30 +41,49 @@ function collectWriteAndSendData() {
     hrm.start();
   }
 
-  // Write data every 5 seconds
   const currentTime = new Date().toTimeString().slice(0, -4);
-  data += currentTime + "," + newdata + "\n"; // Append new data
-  fs.writeFileSync("data.txt", data, "utf-8");
+
+  // Write data to file
+  data += currentTime + "," + newdata + "\n";
+  console.log("Length of data:", data.length);
+
+  // Actually write the data
+  fs.writeFileSync(filename, data, "utf-8");
+
 
   // Check file size and initiate file transfer if the limit is reached
   if (data.length >= fileSizeLimit) {
-    outbox
-      .enqueueFile("/private/data/data.txt")
-      .then(ft => {
-        console.log(`Transfer of ${ft.name} successfully queued.`);
-      })
-      .catch(err => {
-        console.log(`Failed to schedule transfer: ${err}`);
-      });
-    data = ""; // Reset data after sending
+    sendData();
   }
 }
-// Call the function every second
-setInterval(collectWriteAndSendData, 5000);
+
+function sendData() {
+  outbox
+    .enqueueFile("/private/data/data.txt")
+    .then(ft => {
+      console.log(`Transfer of ${ft.name} successfully queued.`);
+    })
+    .catch(err => {
+      console.log(`Failed to schedule transfer: ${err}`);
+    });
+  data = "";
+}
+
+// Append previous collected data
+console.log("If there is a file, load data");
+if (fs.existsSync("data.txt")) {
+  data += fs.readFileSync("data.txt", "utf-8");
+}
+
+console.log(`Model ID:         ${device.modelId}`);
+
+// Enter the loop
+setInterval(collectWriteAndSendData, readInterval);
+
 // Read file content on every clock tick event
 clock.addEventListener("tick", (evt) => {
   if (fs.existsSync("data.txt")) {
     const fileContent = fs.readFileSync("data.txt", "utf-8");
-    myLabel.text = fileContent.length; // Display the length of the file content on the label
+    myLabel.text = fileContent.length + " bytes"; // Display the length of the file content on the label
   }
 });
